e2a619f61ded0647eeb50112ada24aceb59f57d5
GaneshPimpale <pimpale2003@gmail.com>
2020-01-19 18:18:27 -0800

    bruh

---
+++ main.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy
+
+import math
+import time
+
+
+class driverView:
+
+    def __init__(self, ):
+        pass
+
+
+    def find_features(self, ):
+        pass
+
+
+    def driver_masking(self, ):
+        pass
+
+
+    def main(self, ):
+        pass
+
+class autoAim:
+
+    def __init__(self, ):
+        pass
+
+
+    def recognise_line(self, ):
+        pass
+
+
+    def find_angle(self, ):
+        pass
+
+
+    def main(self, ):
+        pass
+        cv2.waitKey(0)
+
