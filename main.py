46279498944d9c153eb974d05f570d9514a0a456
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2020-01-20 22:08:43 -0800

    ver with signal/alarm

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
 import math
-import cv2
+import signal
 import os
+import dotenv
+import cv2
 import numpy as np
 from pathlib import Path
-import dotenv
 from networktables import NetworkTables
 
 dotenv.load_dotenv()
+
 cap = cv2.VideoCapture(-1)
 cap.set(cv2.CAP_PROP_FPS, 1)
 cap.set(cv2.CAP_PROP_EXPOSURE, 10)
 
-left_angle = float(os.getenv('LEFT_ANGLE')) 
-right_angle = float(os.getenv('RIGHT_ANGLE')) 
-bottom_angle = float(os.getenv('BOTTOM_ANGLE')) 
+kernel = np.ones((5,5),np.uint8)
+minLineLength = 5
+maxLineGap = 20
 
 left_angle_stat = (False, 0)
 right_angle_stat = (False, 0)
@@ -28,24 +30,38 @@ def check_slope(cur_slope, check_slope, counter):
     if cur_slope >= (check_slope-float(os.getenv('ANGLE_TOLERANCE'))) and cur_slope <= (check_slope+float(os.getenv('ANGLE_TOLERANCE'))):
         print(f'matches {check_slope} at {cur_slope}')
         return (True, int(os.getenv('MAX_HOLD')))
-        
     elif counter > 0:
         return (True, counter-1)
     else:
         return (False, 0)
 
+def handler(signum, frame):
+    print('Time exceeded')
+    raise Exception('Watchdog Exceeded')
+
+signal.signal(signal.SIGALRM, handler)
+
 while True:
+    left_angle = float(os.getenv('LEFT_ANGLE'))
+    right_angle = float(os.getenv('RIGHT_ANGLE')) 
+    bottom_angle = float(os.getenv('BOTTOM_ANGLE')) 
+    
     frame = cap.read()[1]
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #frame = cv2.blur(frame, (5,5))
+    #TODO inRange for our UV wavelength
+    thresh = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     xsize = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     ysize = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     black = np.zeros((ysize,xsize,3), np.uint8)
-    edges = cv2.Canny(grey, 100, 200)
-    line = grey
-    minLineLength = 5
-    maxLineGap = 20
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, minLineLength, maxLineGap)
+    opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    edges = cv2.Canny(opened, 100, 200)
+
+    signal.alarm(1)
+    try:
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, minLineLength, maxLineGap)
+    except (Exception, exc):
+       print(exc)
+    signal.alarm(0)
+
     if type(lines) != type(None):
         for unpacked_line in lines:
             for x1, y1, x2, y2 in unpacked_line:
@@ -56,8 +72,9 @@ while True:
                 bottom_angle_stat = check_slope(cur_slope, bottom_angle, bottom_angle_stat[1])
     
     if left_angle_stat[0] and right_angle_stat[0] and bottom_angle_stat[0]:
-        print('IT FRICKEN WORKS WHAT THE HELL')
-                
+        print('Target Found.')
+    else: print('Not found.')            
+    
     cv2.imshow('raw_image', frame)
     cv2.imshow('processed_image', edges)
     cv2.imshow('lines', black)
