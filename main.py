8c7fceb5277a68962e50d16c20a8d9d9efa36e8c
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2020-01-20 22:36:02 -0800

    Found true cause of false positives

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import math
-import signal
 import os
 import dotenv
 import cv2
@@ -19,9 +18,9 @@ kernel = np.ones((5,5),np.uint8)
 minLineLength = 5
 maxLineGap = 20
 
-left_angle_stat = (False, 0)
-right_angle_stat = (False, 0)
-bottom_angle_stat = (False, 0)
+left_angle_stat = {'present': False, 'counter': 0}
+right_angle_stat = {'present': False, 'counter': 0}
+bottom_angle_stat = {'present': False, 'counter': 0}
 
 def get_slope(x1, y1, x2, y2):
     return (y2-y1)/(x2-x1)
@@ -29,22 +28,17 @@ def get_slope(x1, y1, x2, y2):
 def check_slope(cur_slope, check_slope, counter):
     if cur_slope >= (check_slope-float(os.getenv('ANGLE_TOLERANCE'))) and cur_slope <= (check_slope+float(os.getenv('ANGLE_TOLERANCE'))):
         print(f'matches {check_slope} at {cur_slope}')
-        return (True, int(os.getenv('MAX_HOLD')))
+        return {'present': True, 'counter': int(os.getenv('MAX_HOLD'))}
     elif counter > 0:
-        return (True, counter-1)
+        return {'present': True, 'counter': counter-1}
     else:
-        return (False, 0)
-
-def handler(signum, frame):
-    print('Time exceeded')
-    raise Exception('Watchdog Exceeded')
-
-signal.signal(signal.SIGALRM, handler)
+        return {'present': False, 'counter': 0}
 
 while True:
     left_angle = float(os.getenv('LEFT_ANGLE'))
     right_angle = float(os.getenv('RIGHT_ANGLE')) 
-    bottom_angle = float(os.getenv('BOTTOM_ANGLE')) 
+    bottom_angle = float(os.getenv('BOTTOM_ANGLE'))
+    stats = [left_angle_stat, right_angle_stat, bottom_angle_stat]
     
     frame = cap.read()[1]
     #TODO inRange for our UV wavelength
@@ -54,24 +48,24 @@ while True:
     black = np.zeros((ysize,xsize,3), np.uint8)
     opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
     edges = cv2.Canny(opened, 100, 200)
-
-    signal.alarm(1)
-    try:
-        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, minLineLength, maxLineGap)
-    except (Exception, exc):
-       print(exc)
-    signal.alarm(0)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, minLineLength, maxLineGap)
 
     if type(lines) != type(None):
         for unpacked_line in lines:
             for x1, y1, x2, y2 in unpacked_line:
                 cv2.line(black, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cur_slope = get_slope(x1, y1, x2, y2)
-                left_angle_stat = check_slope(cur_slope, left_angle, left_angle_stat[1])
-                right_angle_stat = check_slope(cur_slope, right_angle, right_angle_stat[1])
-                bottom_angle_stat = check_slope(cur_slope, bottom_angle, bottom_angle_stat[1])
-    
-    if left_angle_stat[0] and right_angle_stat[0] and bottom_angle_stat[0]:
+                left_angle_stat = check_slope(cur_slope, left_angle, left_angle_stat['counter'])
+                right_angle_stat = check_slope(cur_slope, right_angle, right_angle_stat['counter'])
+                bottom_angle_stat = check_slope(cur_slope, bottom_angle, bottom_angle_stat['counter'])
+    else:
+        for stat in stats:
+            if stat['counter'] > 0:
+                stat['counter'] -= 1
+            else:
+                stat['present'] = False
+                stat['counter'] = 0
+    if all(item['present'] for item in stats):
         print('Target Found.')
     else: print('Not found.')            
     
