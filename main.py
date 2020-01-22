49fec324e77a48c5649725b2510ea6c5e780025b
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2020-01-22 14:53:27 -0800

    Added network tables

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import math
 import os
+import threading
 import dotenv
 import cv2
 import numpy as np
@@ -38,20 +39,49 @@ def check_slope(cur_slope, check_slope, counter):
     else:
         return ({'present': False, 'counter': 0}, False)
 
+def connect():
+    cond = threading.Condition()
+    notified = [False]
+
+    def connectionListener(connected, info):
+        print(info, '; Connected=%s' % connected)
+        with cond:
+            notified[0] = True
+            cond.notify()
+
+    NetworkTables.initialize(server='10.26.43.2')
+    NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+    with cond:
+        print("Waiting")
+        if not notified[0]:
+            cond.wait()
+
+    return NetworkTables.getTable('Vision')
+
+if os.getenv('CONNECT_TO_SERVER') == 'True':
+    table = connect()
+    
+x_size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+y_size = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(f'The current image is {x_size}, {y_size}')
+
 while True:
     verbosity = int(os.getenv('VERBOSITY'))
     left_angle = float(os.getenv('LEFT_ANGLE'))
     right_angle = float(os.getenv('RIGHT_ANGLE')) 
     bottom_angle = float(os.getenv('BOTTOM_ANGLE'))
+    x_target = int(os.getenv('X_TARGET'))
+    y_target = int(os.getenv('Y_TARGET'))
     stats = [left_angle_stat, right_angle_stat, bottom_angle_stat]
     check_vals = [left_angle, right_angle, bottom_angle]
     
     frame = cap.read()[1]
     #TODO inRange for our UV wavelength
     thresh = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    xsize = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    ysize = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    black = np.zeros((ysize,xsize,3), np.uint8)
+    x_size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    y_size = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    black = np.zeros((y_size,x_size,3), np.uint8)
     opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
     edges = cv2.Canny(opened, 100, 200)
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, minLineLength, maxLineGap)
@@ -81,16 +111,28 @@ while True:
     if all(item['present'] for item in stats):
         if verbosity >= 2:
             print('Target Found.')
+        
         x_vals = [validpts[index]['x'][index_two] for index in range(len(validpts)) for index_two in range(len(validpts[index]['x']))]
         x_avg = int(sum(x_vals)/len(x_vals))
         y_vals = [validpts[index]['y'][index_two] for index in range(len(validpts)) for index_two in range(len(validpts[index]['y']))]
         offset_to_middle = int(max(y_vals)-min(y_vals))
         y_avg = int(sum(y_vals)/len(y_vals))
         target_position = (x_avg, y_avg-offset_to_middle)
+
+        if os.getenv('CONNECT_TO_SERVER') == 'True':
+            table.putBoolean('valid', True)
+            table.putNumber('x', target_position[0])
+            table.putNumber('y', target_position[1])
+            table.putNumber('x_offset', (target_position[0]-x_target))
+            table.putNumber('y_offset', (target_position[1]-y_target))
+
         cv2.circle(black, target_position, 5, (0,255,0))
     elif verbosity >= 2:
-        print('Not found.')            
+        print('Not found.')
+        if os.getenv('CONNECT_TO_SERVER') == 'True':  
+            table.putBoolean('valid', False)          
     
+    cv2.circle(black, (x_target, y_target), 5, (0,0,255))
     if verbosity >= 1:
         cv2.imshow('raw_image', frame)
         cv2.imshow('processed_image', edges)
