055a2c5ec166972226b3b57bf3fbfa02dff9ad6c
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2020-01-28 17:05:45 -0800

    half-broken, but targets are put into application memory

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ from networktables import NetworkTables
 
 config_parser = configparser.ConfigParser()
 config_parser.read('config.conf')
-target = config_parser['2019-High-Target']
 config = config_parser['SETTINGS']
+verbosity = config.getint('VERBOSITY')
+target_list = list(config_parser.sections())
+target_list.remove('SETTINGS')
 
 cap = cv2.VideoCapture(-1)
 cap.set(cv2.CAP_PROP_FPS, 1)
@@ -31,9 +33,9 @@ def get_slope(x1, y1, x2, y2):
     return (y2-y1)/(x2-x1)
 
 
-def check_slope(cur_slope, check_slope, counter):
+def check_slope(target, cur_slope, check_slope, counter):
     """Returns acceptability and whether that run was truely valid"""
-    if cur_slope >= (check_slope-target.getfloat('ANGLE_TOLERANCE')) and cur_slope <= (check_slope+target.getfloat('ANGLE_TOLERANCE')):
+    if cur_slope >= (check_slope-target['ANGLE_TOLERANCE']) and cur_slope <= (check_slope+target.getfloat('ANGLE_TOLERANCE')):
         global verbosity
         if verbosity >= 3:
             print(f'matches {check_slope} at {cur_slope}')
@@ -45,18 +47,18 @@ def check_slope(cur_slope, check_slope, counter):
 
 
 def connect():
-    cond=threading.Condition()
-    notified=[False]
+    cond = threading.Condition()
+    notified = [False]
 
     def connectionListener(connected, info):
         print(info, '; Connected=%s' % connected)
         with cond:
-            notified[0]=True
+            notified[0] = True
             cond.notify()
 
-    NetworkTables.initialize(server = '10.26.43.2')
+    NetworkTables.initialize(server='10.26.43.2')
     NetworkTables.addConnectionListener(
-        connectionListener, immediateNotify = True)
+        connectionListener, immediateNotify=True)
 
     with cond:
         print("Waiting")
@@ -67,46 +69,54 @@ def connect():
 
 
 if config.getboolean('CONNECT_TO_SERVER'):
-    table=connect()
+    table = connect()
 
-x_size=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-y_size=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+x_size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+y_size = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(f'The current image is {x_size}, {y_size}')
 
-while True:
-    verbosity=config.getint('VERBOSITY')
-    left_angle=target.getfloat('LEFT_ANGLE')
-    right_angle=target.getfloat('RIGHT_ANGLE')
-    bottom_angle=target.getfloat('BOTTOM_ANGLE')
-    x_target=target.getint('X_TARGET')
-    y_target=target.getint('Y_TARGET')
-    stats=[left_angle_stat, right_angle_stat, bottom_angle_stat]
-    check_vals=[left_angle, right_angle, bottom_angle]
+targets = {}
+for target in target_list:
+    targets[target] = {}
+    targets[target]['angle_names'] = config_parser[target]['ANGLE_NAMES'].split()
+    targets[target]['angle_tolerance'] = config_parser[target].getfloat('ANGLE_TOLERANCE')
+    targets[target]['max_hold'] = config_parser[target].getint('MAX_HOLD')
+    targets[target]['x_target'] = config_parser[target].getint('X_TARGET')
+    targets[target]['y_target'] = config_parser[target].getint('Y_TARGET')
+    targets[target]['valid_points'] = {}
+    for angle_name in targets[target]['angle_names']:
+        targets[target][angle_name] = {'angle': config_parser[target].getfloat(
+            angle_name), 'counter': 0}
 
-    frame=cap.read()[1]
+while True:
+    frame = cap.read()[1]
     # TODO inRange for our UV wavelength
-    thresh=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    x_size=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    y_size=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    black=np.zeros((y_size, x_size, 3), np.uint8)
-    opened=cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    edges=cv2.Canny(opened, 100, 200)
-    lines=cv2.HoughLinesP(edges, 1, np.pi/180, 10, minLineLength, maxLineGap)
+    # thresh = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV),
+    #                              (113, 89, 0), # These values are for 4
+    #                              (123, 255, 80))
+    thresh = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    x_size = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    y_size = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    black = np.zeros((y_size, x_size, 3), np.uint8)
+    opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    edges = cv2.Canny(opened, 100, 200)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, minLineLength, maxLineGap)
 
     if type(lines) != type(None):
         for unpacked_line in lines:
             for x1, y1, x2, y2 in unpacked_line:
                 cv2.line(black, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cur_slope=get_slope(x1, y1, x2, y2)
-                for index in range(len(stats)):
-                    return_value=check_slope(
-                        cur_slope, check_vals[index], stats[index]['counter'])
-                    stats[index]=return_value[0]
-                    if return_value[1]:
-                        validpts[index]['x'].extend((x1, x2))
-                        validpts[index]['y'].extend((y1, y2))
-                    elif not stats[index]['present']:
-                        validpts[index]={'x': [], 'y': []}
+                cur_slope = get_slope(x1, y1, x2, y2)
+                for target in targets:
+                    for angle in target['angle_names']:
+                        check_slope(
+                            target, cur_slope, target[angle], target[angle]['counter'])
+                        stats[index] = return_value[0]
+                        if return_value[1]:
+                            validpts[index]['x'].extend((x1, x2))
+                            validpts[index]['y'].extend((y1, y2))
+                        elif not stats[index]['present']:
+                            validpts[index] = {'x': [], 'y': []}
 
     else:
         for stat in stats:
